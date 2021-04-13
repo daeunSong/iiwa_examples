0df601376ac152136ef63290bde388172eccccf9
daeunSong <daeun7250@gmail.com>
2021-04-13 15:12:39 +0900

    Fix updown example to init iiwa_ros

--- src/updown_moveit.cpp
+++ src/updown_moveit.cpp
@@ -1,105 +1,71 @@
-#include "iiwa_ros/iiwa_ros.hpp"
+#include "ros/ros.h"
+#include <iiwa_ros/state/cartesian_pose.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-#include <moveit_visual_tools/moveit_visual_tools.h>
-#include <ros/package.h>
-#include <cmath>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <vector>
 
-
-using namespace std;
 using moveit::planning_interface::MoveItErrorCode;
 
 // Create MoveGroup
 static const std::string PLANNING_GROUP = "manipulator";
 static const std::string EE_LINK = "iiwa_link_ee";
 
-bool sim;
-
-vector<string> split(string input, char delimiter){
-    vector<string> ans;
-    stringstream str(input);
-    string temp;
-    
-    while(getline(str, temp, delimiter)){
-        ans.push_back(temp);
-    }
-    
-    return ans;
-}
-
 int main (int argc, char **argv) {
-    // Initialize ROS
-    ros::init(argc, argv, "UpdownMoveit");
-    ros::NodeHandle nh("~");
-    nh.param("sim", sim, true);
 
+  // Initialize ROS
+  ros::init(argc, argv, "CommandRobotMoveit");
+  ros::NodeHandle nh("~");
 
-    // ROS spinner.
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
-    
-    std::string movegroup_name, ee_link;
-    geometry_msgs::PoseStamped current_cartesian_position, command_cartesian_position, start, end;
-    std::string joint_position_topic, cartesian_position_topic;
-    std::vector<geometry_msgs::Pose> drawing_stroke;
-    std::vector<geometry_msgs::Pose> linear_path;
-    geometry_msgs::Pose drawing_point;
-    geometry_msgs::Pose path_point;
+  // ROS spinner.
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
 
-    // Dynamic parameters. Last arg is the default value. You can assign these from a launch file.
-    nh.param<std::string>("move_group", movegroup_name, PLANNING_GROUP);
-    nh.param<std::string>("ee_link", ee_link, EE_LINK);    
+  iiwa_ros::state::CartesianPose iiwa_pose_state;
+  iiwa_pose_state.init("iiwa");
 
-    // Create Move Group
-    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+  std::string movegroup_name, ee_link;
+  geometry_msgs::PoseStamped command_cartesian_position;
 
-    // Configure Move Group
-    move_group.setPlanningTime(0.5);
-    move_group.setPlannerId(PLANNING_GROUP+"[RRTConnectkConfigDefault]");
-    move_group.setEndEffectorLink(ee_link);
-    ROS_INFO("Planning frame: %s", move_group.getPlanningFrame().c_str());
-    ROS_INFO("End effector link: %s", move_group.getEndEffectorLink().c_str());
+  // Dynamic parameters. Last arg is the default value. You can assign these from a launch file.
+  nh.param<std::string>("move_group", movegroup_name, PLANNING_GROUP);
+  nh.param<std::string>("ee_link", ee_link, EE_LINK);
 
-    // Dynamic parameter to choose the rate at wich this node should run
-    double ros_rate;
-    nh.param("ros_rate", ros_rate, 0.1); // 0.1 Hz = 10 seconds
-    ros::Rate* loop_rate_ = new ros::Rate(ros_rate);
+  // Dynamic parameter to choose the rate at wich this node should run
+  double ros_rate;
+  nh.param("ros_rate", ros_rate, 0.1); // 0.1 Hz = 10 seconds
+  ros::Rate* loop_rate_ = new ros::Rate(ros_rate);
 
-    int direction = 1;
-    MoveItErrorCode success_plan = MoveItErrorCode::FAILURE, motion_done = MoveItErrorCode::FAILURE;
-    
-    while (ros::ok()){
-        ros::Duration(5).sleep(); // wait for 2 sec
-        ROS_INFO("Sleeping 5 seconds before starting ... ");
+  int direction = 1;
 
-        command_cartesian_position = move_group.getCurrentPose(ee_link);   
-        command_cartesian_position.pose.position.z -= direction * 0.10;
+  // Create MoveGroup
+  moveit::planning_interface::MoveGroupInterface group(movegroup_name);
+  moveit::planning_interface::MoveGroupInterface::Plan myplan;
 
-        move_group.setStartStateToCurrentState();
-        move_group.setPoseTarget(command_cartesian_position, ee_link);
-        success_plan = move_group.plan(my_plan);
-        if (success_plan == MoveItErrorCode::SUCCESS) {
-            motion_done = move_group.execute(my_plan);
-        }
-        if (motion_done == MoveItErrorCode::SUCCESS) {
-            direction *= -1; // In the next iteration the motion will be on the opposite direction
-            loop_rate_->sleep(); // Sleep for some millisecond. The while loop will run every 10 seconds in this example.
-        }
-    }
+  // Configure planner
+  group.setPlanningTime(0.5);
+  group.setPlannerId("RRTConnectkConfigDefault");
+  group.setEndEffectorLink(ee_link);
+  MoveItErrorCode success_plan = MoveItErrorCode::FAILURE, motion_done = MoveItErrorCode::FAILURE;
+  while (ros::ok()) {
+    // if (iiwa_pose_state.isConnected()) {
 
-    cerr<<"Stopping spinner..."<<endl;
-    spinner.stop();
+      command_cartesian_position = group.getCurrentPose(ee_link);
+      command_cartesian_position.pose.position.z -= direction * 0.10;
 
-    cerr<<"Bye!"<<endl;
+      group.setStartStateToCurrentState();
+      group.setPoseTarget(command_cartesian_position, ee_link);
+      success_plan = group.plan(myplan);
+      if (success_plan == MoveItErrorCode::SUCCESS) {
+        motion_done = group.execute(myplan);
+      }
+      if (motion_done == MoveItErrorCode::SUCCESS) {
+        direction *= -1; // In the next iteration the motion will be on the opposite direction
+        loop_rate_->sleep(); // Sleep for some millisecond. The while loop will run every 10 seconds in this example.
+      }
+    // }
+    // else {
+    //   ROS_WARN_STREAM("Robot is not connected...");
+    //   ros::Duration(5.0).sleep(); // 5 seconds
+    // }
+  }
 
-    return 0;
+  return 0;
 }
